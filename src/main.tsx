6f283b4be1eb72051b483c8f8454820eee6a93b0
Steve Peak <steve@stevepeak.net>
2025-05-16 08:59:00 -0600

    feat(dependencies): add Sentry for error tracking and history for navigation
    Integrate Sentry for error tracking by adding the @sentry/react and @sentry/tracing packages. Initialize Sentry in the main application file with a DSN and configuration for tracing. Additionally, include the history package to manage navigation history effectively. This update enhances the application's monitoring capabilities and improves navigation handling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,29 @@
+import * as Sentry from "@sentry/react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import React from "react";
+import {
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router-dom";
+
+Sentry.init({
+  dsn: "https://f0b39040299b245a914fdd95a6425c8b@o4504148959756288.ingest.us.sentry.io/4509333190148096",
+  integrations: [
+    // Sentry.reactRouterV6BrowserTracingIntegration({
+    //   useEffect: React.useEffect,
+    //   useLocation,
+    //   useNavigationType,
+    //   createRoutesFromChildren,
+    //   matchRoutes,
+    // }),
+    // Sentry.browserTracingIntegration(),
+  ],
+  sendDefaultPii: true,
+  tracesSampleRate: 1.0, // Adjust this value in production
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
